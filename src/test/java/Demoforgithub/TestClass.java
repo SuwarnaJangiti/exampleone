83827e5c8bb7fc28ccfe68fa73bc8fef4490a31e
suwarna <suwarna@naarsoft.com>
2023-11-14 13:13:51 +0530

    third commit

--- src/test/java/Demoforgithub/TestClass.java
+++ src/test/java/Demoforgithub/TestClass.java
@@ -1,10 +1,10 @@
 package Demoforgithub;
 
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.chrome.ChromeOptions;
-//import org.testng.Assert;
-//import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -12,20 +12,20 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("hellow world");
 	}
-//	WebDriver driver;
-//
-//	@Test
-//	public void verifyTitle() {
-//	//WebDriverManager.chromedriver().setup();
-//	ChromeOptions options=new ChromeOptions();
-//	options.addArguments("headless");
-//	WebDriverManager.chromedriver().setup();
-//	driver=new ChromeDriver(options);
-//	driver.get("https://www.browserstack.com/");
-//	System.out.println("Title is: " +driver.getTitle());
-//	Assert.assertEquals(driver.getTitle(), "Most Reliable App & Cross Browser Testing Platform | BrowserStack");
-//	driver.quit();
-//	}
+	WebDriver driver;
+
+	@Test
+	public void verifyTitle() {
+	//WebDriverManager.chromedriver().setup();
+	ChromeOptions options=new ChromeOptions();
+	options.addArguments("headless");
+	WebDriverManager.chromedriver().setup();
+	driver=new ChromeDriver(options);
+	driver.get("https://www.browserstack.com/");
+	System.out.println("Title is: " +driver.getTitle());
+	Assert.assertEquals(driver.getTitle(), "Most Reliable App & Cross Browser Testing Platform | BrowserStack");
+	driver.quit();
+	}
 	
 
 }
