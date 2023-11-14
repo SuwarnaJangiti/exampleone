6dbe3d50bee2f38da16a347d06194be29a7b28ee
suwarna <suwarna@naarsoft.com>
2023-11-14 13:18:58 +0530

    fourth commit

--- src/test/java/Demoforgithub/TestClass.java
+++ src/test/java/Demoforgithub/TestClass.java
@@ -9,9 +9,9 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestClass {
-	public static void main(String[] args) {
-		System.out.println("hellow world");
-	}
+//	public static void main(String[] args) {
+//		System.out.println("hellow world");
+//	}
 	WebDriver driver;
 
 	@Test
@@ -21,9 +21,8 @@ public class TestClass {
 	options.addArguments("headless");
 	WebDriverManager.chromedriver().setup();
 	driver=new ChromeDriver(options);
-	driver.get("https://www.browserstack.com/");
+	driver.get("https://www.google.com/");
 	System.out.println("Title is: " +driver.getTitle());
-	Assert.assertEquals(driver.getTitle(), "Most Reliable App & Cross Browser Testing Platform | BrowserStack");
 	driver.quit();
 	}
 	
